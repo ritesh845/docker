9902008d9fc6777327ce4977a649bab766e25381
Ritesh Panchal <ritesh.panchal@esparkbizmail.com>
2022-08-30 18:17:24 +0530

    install sh

--- exam-docker/install.sh
+++ exam-docker/install.sh
@@ -5,6 +5,8 @@ then
         cd frontend
         echo "Frontend app url :"
         git clone -b develop https://gitlab.com/espark-exam/frontend.git .
+        cd ..
+        sudo chmod -R 777 frontend
     fi
 else
     if [ -f "frontend" ]
@@ -14,6 +16,10 @@ else
     mkdir -p frontend
     cd frontend 
     git clone -b develop https://gitlab.com/espark-exam/frontend.git .
+
+    cd ..
+    sudo chmod -R 777 frontend
+    
 fi
 
 if [ -e "backend" -a -d "backend" ]
@@ -22,6 +28,8 @@ then
     then
         cd backend
         git clone -b develop https://gitlab.com/espark-exam/api.git .
+        cd ..
+        sudo chmod -R 777 backend
     fi
 else
     if [ -f "backend" ]
@@ -31,6 +39,8 @@ else
     mkdir -p backend
     cd backend 
     git clone -b develop https://gitlab.com/espark-exam/api.git .
+    cd ..
+    sudo chmod -R 777 backend
 fi
 
 if [ -e "mysql" -a -d "mysql" ]
@@ -42,4 +52,5 @@ else
         rm -rf mysql
     fi
     mkdir  mysql
+    sudo chmod -R 777 mysql
 fi
